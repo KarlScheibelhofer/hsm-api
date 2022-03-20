7a123b5f56ea52dda3a1f16aff0ca29cece276d6
KarlScheibelhofer <karl.scheibelhofer@gmx.net>
2022-03-20 15:41:41 +0100

    add note

--- src/test/java/at/karl/hsm/NativeKeyResourceIT.java
+++ src/test/java/at/karl/hsm/NativeKeyResourceIT.java
@@ -2,6 +2,10 @@ package at.karl.hsm;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+/**
+ * Integration tests can only test the external API of the application 
+ * because these tests run in a separate process. 
+ */
 @NativeImageTest
 public class NativeKeyResourceIT extends KeyResourceTest {
 
