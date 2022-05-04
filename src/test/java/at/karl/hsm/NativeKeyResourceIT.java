1967ff821fb26b889c13731a60e0449fd1e76f56
KarlScheibelhofer <karl.scheibelhofer@gmx.net>
2022-05-04 09:49:00 +0200

    replace deprecated @NativeImageTest

--- src/test/java/at/karl/hsm/NativeKeyResourceIT.java
+++ src/test/java/at/karl/hsm/NativeKeyResourceIT.java
@@ -1,12 +1,12 @@
 package at.karl.hsm;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 /**
  * Integration tests can only test the external API of the application 
  * because these tests run in a separate process. 
  */
-@NativeImageTest
+@QuarkusIntegrationTest
 public class NativeKeyResourceIT extends KeyResourceTest {
 
     // Execute the same tests but in native mode.
